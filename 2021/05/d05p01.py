a571e79fa6c339041ebabf2cac47b73e2e519ac1
Kobe Peeters <kobe.peeters@ingka.com>
2023-11-19 01:20:34 +0100

    day05 fixed

--- 2021/05/d05p01.py
+++ 2021/05/d05p01.py
@@ -5,23 +5,26 @@ def parseLine(line):
     y1 = int(arr[0].split(",")[1])
     x2 = int(arr[2].split(",")[0])
     y2 = int(arr[2].split(",")[1])
-    return(x1, y1, x2, y2)
-    #return (min(x1,x2), min(y1,y2), max(x1,x2), max(y1,y2))
+    return (x1, y1, x2, y2)
 
 def findHighestX(lines):
     hightestX = 0
     for line in lines:
         x1, y1, x2, y2 = line
-        if max(int(x1),int(x2)) > hightestX:
-            hightestX = max(int(x1),int(x2))
+        if int(x2) > hightestX:
+            hightestX = int(x2)
+        if int(x1) > hightestX:
+            hightestX = int(x1)
     return hightestX
 
 def findHighestY(lines):
     hightestY = 0
     for line in lines:
         x1, y1, x2, y2 = line
-        if max(int(y1),int(y2)) > hightestY:
-            hightestY = max(int(y1),int(y2))
+        if int(y2) > hightestY:
+            hightestY = int(y2)
+        if int(y1) > hightestY:
+            hightestY = int(y1)
     return hightestY
 
 def createGrid(x, y):
@@ -43,12 +46,14 @@ def drawLine(grid, line):
     if(x1 == x2 ):
         if y1 < y2:
             Y_incr = 1
-            yMax = x2+1
+            yMin = y1
+            yMax = y2+1
         else:
             Y_incr = -1
-            yMax = x1-1
+            yMin = y1
+            yMax = y2-1
         print("Drawing Vertical line [x1,y1,x2,y2]: ", line, Y_incr)
-        (X, Y) = (x1, y1)
+        (X, Y) = (x1, yMin)
         while True:
             print("grid[x1][Y] = ", x1,Y)
             grid[x1][Y] += 1
@@ -58,12 +63,14 @@ def drawLine(grid, line):
     if(y1 == y2):
         if x1 < x2:
             X_incr = 1
+            xMin = x1
             xMax = x2+1
         else:
             X_incr = -1
-            xMax = x1-1
+            xMin = x1
+            xMax = x2-1
         print("Drawing Horizontal line [x1,y1,x2,y2]: ", line, X_incr)
-        (X, Y) = (x1, y1)
+        (X, Y) = (xMin, y1)
         while True:
             print("grid[X][y1] = ", X,y1)
             grid[X][y1] += 1
@@ -71,17 +78,7 @@ def drawLine(grid, line):
             if (X) == (xMax):
                 return grid
     else:
-        print("Diagonal")
-        return grid
-        X_incr = 1 if x1 < x2 else -1
-        Y_incr = 1 if y1 < y2 else -1
-        (X, Y) = (x1, y1)
-        while True:
-            X += X_incr
-            Y += Y_incr
-            grid[X][Y] += 1
-            if (X, Y) == (x2, y2):
-                break
+        print("Diagonal, Skipping")
         return grid
 
 def countDangerousPoints(grid,maxX,maxY):
--- 2021/05/d05p02.py
+++ 2021/05/d05p02.py
@@ -5,23 +5,26 @@ def parseLine(line):
     y1 = int(arr[0].split(",")[1])
     x2 = int(arr[2].split(",")[0])
     y2 = int(arr[2].split(",")[1])
-    return(x1, y1, x2, y2)
-    #return (min(x1,x2), min(y1,y2), max(x1,x2), max(y1,y2))
+    return (x1, y1, x2, y2)
 
 def findHighestX(lines):
     hightestX = 0
     for line in lines:
         x1, y1, x2, y2 = line
-        if max(int(x1),int(x2)) > hightestX:
-            hightestX = max(int(x1),int(x2))
+        if int(x2) > hightestX:
+            hightestX = int(x2)
+        if int(x1) > hightestX:
+            hightestX = int(x1)
     return hightestX
 
 def findHighestY(lines):
     hightestY = 0
     for line in lines:
         x1, y1, x2, y2 = line
-        if max(int(y1),int(y2)) > hightestY:
-            hightestY = max(int(y1),int(y2))
+        if int(y2) > hightestY:
+            hightestY = int(y2)
+        if int(y1) > hightestY:
+            hightestY = int(y1)
     return hightestY
 
 def createGrid(x, y):
@@ -43,12 +46,14 @@ def drawLine(grid, line):
     if(x1 == x2 ):
         if y1 < y2:
             Y_incr = 1
-            yMax = x2+1
+            yMin = y1
+            yMax = y2+1
         else:
             Y_incr = -1
-            yMax = x1-1
+            yMin = y1
+            yMax = y2-1
         print("Drawing Vertical line [x1,y1,x2,y2]: ", line, Y_incr)
-        (X, Y) = (x1, y1)
+        (X, Y) = (x1, yMin)
         while True:
             print("grid[x1][Y] = ", x1,Y)
             grid[x1][Y] += 1
@@ -58,12 +63,14 @@ def drawLine(grid, line):
     if(y1 == y2):
         if x1 < x2:
             X_incr = 1
+            xMin = x1
             xMax = x2+1
         else:
             X_incr = -1
-            xMax = x1-1
+            xMin = x1
+            xMax = x2-1
         print("Drawing Horizontal line [x1,y1,x2,y2]: ", line, X_incr)
-        (X, Y) = (x1, y1)
+        (X, Y) = (xMin, y1)
         while True:
             print("grid[X][y1] = ", X,y1)
             grid[X][y1] += 1
@@ -71,16 +78,31 @@ def drawLine(grid, line):
             if (X) == (xMax):
                 return grid
     else:
-        print("Diagonal")
-        return grid
-        X_incr = 1 if x1 < x2 else -1
-        Y_incr = 1 if y1 < y2 else -1
+        if x1 < x2:
+            X_incr = 1
+            xMin = x1
+            xMax = x2+1
+        else:
+            X_incr = -1
+            xMin = x1
+            xMax = x2-1
+        if y1 < y2:
+            Y_incr = 1
+            yMin = y1
+            yMax = y2+1
+        else:
+            Y_incr = -1
+            yMin = y1
+            yMax = y2-1
+        print("Drawing Diagonal line [x1,y1,x2,y2]: ", line, X_incr, Y_incr)
         (X, Y) = (x1, y1)
         while True:
+            print("grid[X][Y] = ", X,Y)
+            grid[X][Y] += 1
             X += X_incr
             Y += Y_incr
-            grid[X][Y] += 1
-            if (X, Y) == (x2, y2):
+            print(X,Y,xMax,yMax)
+            if (X, Y) == (xMax, yMax):
                 break
         return grid
 
@@ -92,7 +114,7 @@ def countDangerousPoints(grid,maxX,maxY):
                 count += 1
     return count
 
-with open("test.input", "r") as f:
+with open("d05p01.input", "r") as f:
     content = f.readlines()
     lines = [parseLine(line) for line in content]
     maxX = findHighestX(lines)
